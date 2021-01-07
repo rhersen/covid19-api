f2cb09dc67b4e16ddfc5207f5271598a6a3de144
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-01-07 15:07:47 +0100

    returns .xlsx file

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
+import axios from "axios";
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  axios.get('https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data', {
+    responseType: 'arraybuffer',
+  })
+    .then(function(response) {
+      console.log('handle success', response.status, response.statusText, response.data.length, 'bytes')
+      res.writeHead(200, {
+        'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'Content-disposition': 'attachment;filename=Folkhalsomyndigheten_Covid19.xlsx',
+        'Content-Length': response.data.length
+      });
+      res.end(Buffer.from(response.data, 'binary'));
+    })
+    .catch(function(error) {
+      console.log('handle error')
+      console.log(error);
+    })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`covid19-api app listening at http://localhost:${port}`)
 })
