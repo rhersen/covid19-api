0f993c00b640026806bb7b67e389a10a554238be
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-01-07 17:02:52 +0100

    add endpoint "/json"

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-import express from "express";
 import axios from "axios";
+import express from "express";
+import xlsx from "xlsx";
+
 const app = express()
 const port = 3000
+const url = 'https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data';
 
-app.get('/', (req, res) => {
-  axios.get('https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data', {
-    responseType: 'arraybuffer',
-  })
-    .then(function({data, status, statusText}) {
-      console.log('handle success', status, statusText, data.length, 'bytes')
-      res.writeHead(200, {
-        'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        'Content-disposition': 'attachment;filename=Folkhalsomyndigheten_Covid19.xlsx',
-        'Content-Length': data.length
-      });
-      res.end(Buffer.from(data, 'binary'));
-    })
-    .catch(function(error) {
-      console.log('handle error')
-      console.log(error);
-    })
+const respondWithJson = res => ({data, status, statusText}) => {
+  console.log(status, statusText, data.length, 'bytes')
+  res.send(xlsx.read(data));
+};
+
+const respondWithXlsx = res => ({data}) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'Content-disposition': 'attachment;filename=Folkhalsomyndigheten_Covid19.xlsx',
+    'Content-Length': data.length
+  });
+  res.end(Buffer.from(data, 'binary'));
+};
+
+const handleError = res => error => {
+  console.error(error);
+  res.status(500).send(error.message)
+};
+
+app.get('/xlsx', (req, res) => {
+  axios.get(url, {responseType: 'arraybuffer',})
+    .then(respondWithXlsx(res))
+    .catch(handleError(res))
+})
+
+app.get('/json', (req, res) => {
+  axios.get(url, {responseType: 'arraybuffer',})
+    .then(respondWithJson(res))
+    .catch(handleError(res))
 })
 
 app.listen(port, () => {
