4289bd0308fd01e10999b4a20af1242b3ca76cb9
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-01-07 17:05:41 +0100

    prettier

--- index.js
+++ index.js
@@ -2,41 +2,46 @@ import axios from "axios";
 import express from "express";
 import xlsx from "xlsx";
 
-const app = express()
-const port = 3000
-const url = 'https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data';
+const app = express();
+const port = 3000;
+const url =
+  "https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data";
 
-const respondWithJson = res => ({data, status, statusText}) => {
-  console.log(status, statusText, data.length, 'bytes')
+const respondWithJson = (res) => ({ data, status, statusText }) => {
+  console.log(status, statusText, data.length, "bytes");
   res.send(xlsx.read(data));
 };
 
-const respondWithXlsx = res => ({data}) => {
+const respondWithXlsx = (res) => ({ data }) => {
   res.writeHead(200, {
-    'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    'Content-disposition': 'attachment;filename=Folkhalsomyndigheten_Covid19.xlsx',
-    'Content-Length': data.length
+    "Content-Type":
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "Content-disposition":
+      "attachment;filename=Folkhalsomyndigheten_Covid19.xlsx",
+    "Content-Length": data.length,
   });
-  res.end(Buffer.from(data, 'binary'));
+  res.end(Buffer.from(data, "binary"));
 };
 
-const handleError = res => error => {
+const handleError = (res) => (error) => {
   console.error(error);
-  res.status(500).send(error.message)
+  res.status(500).send(error.message);
 };
 
-app.get('/xlsx', (req, res) => {
-  axios.get(url, {responseType: 'arraybuffer',})
+app.get("/xlsx", (req, res) => {
+  axios
+    .get(url, { responseType: "arraybuffer" })
     .then(respondWithXlsx(res))
-    .catch(handleError(res))
-})
+    .catch(handleError(res));
+});
 
-app.get('/json', (req, res) => {
-  axios.get(url, {responseType: 'arraybuffer',})
+app.get("/json", (req, res) => {
+  axios
+    .get(url, { responseType: "arraybuffer" })
     .then(respondWithJson(res))
-    .catch(handleError(res))
-})
+    .catch(handleError(res));
+});
 
 app.listen(port, () => {
-  console.log(`covid19-api app listening at http://localhost:${port}`)
-})
+  console.log(`covid19-api app listening at http://localhost:${port}`);
+});
