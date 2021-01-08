84b139c6733b26cbdddc6f0345fcb6e3344bc52f
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-01-08 10:31:10 +0100

    cleanup

--- src/regions.js
+++ src/regions.js
@@ -1,24 +1,29 @@
 export default (sheet) => {
   let entries = Object.entries(sheet);
-  let columns = Object.fromEntries(
+
+  let columnKeys = Object.fromEntries(
     entries
       .filter(([cell]) => /^[^A]1$/.test(cell))
       .map(([cell, value]) => [/^(\D+)(\d+)$/.exec(cell)[1], value.v])
   );
-  let rows = Object.fromEntries(
+
+  let rowKeys = Object.fromEntries(
     entries
       .filter(([cell]) => /^A\d+$/.test(cell))
       .filter(([cell]) => cell !== "A1")
       .map(([cell, value]) => [/^(\D+)(\d+)$/.exec(cell)[2], value.w])
   );
-  let r = {};
-  Object.entries(columns).forEach(([, value]) => (r[value] = {}));
+
+  let columns = Object.fromEntries(
+    Object.entries(columnKeys).map(([, value]) => [value, {}])
+  );
+
   entries
     .filter(([cell]) => !/^A/.test(cell))
     .filter(([cell]) => !/\D1$/.test(cell))
     .forEach(([cell, value]) => {
       let match = /^(\D+)(\d+)$/.exec(cell);
-      if (match) r[columns[match[1]]][rows[match[2]]] = value.v;
+      if (match) columns[columnKeys[match[1]]][rowKeys[match[2]]] = value.v;
     });
-  return r;
+  return columns;
 };
